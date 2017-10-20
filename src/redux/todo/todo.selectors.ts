ab57c52dfc50d26a79225d38649b2fe69807533b
Nicolas Molina Monroy <nicolas.molina.monroy@gmail.com>
2017-10-20 00:01:08 -0400

    :hammer: refactor getVisibleTodos

--- src/redux/todo/todo.selectors.ts
+++ src/redux/todo/todo.selectors.ts
@@ -1,20 +1,26 @@
 import { createSelector } from '@ngrx/store';
 import { AppState } from './../app.reducer';
+import { Todo } from './todo.model';
 
-export const getState = (state: AppState) => state;
-export const getTodos = (state: AppState) => state.todos;
+export const getState  = (state: AppState) => state;
+export const getFilter = (state: AppState) => state.filter;
+export const getTodos  = (state: AppState) => state.todos;
 
-export const getVisibleTodos = createSelector(getState, (state: AppState) => {
-  switch (state.filter) {
-    default:
-    case 'SHOW_ALL':
-      return state.todos;
-    case 'SHOW_COMPLETED':
-      return state.todos.filter(t => t.completed);
-    case 'SHOW_ACTIVE':
-      return state.todos.filter(t => !t.completed);
+export const getVisibleTodos = createSelector(
+  getTodos,
+  getFilter,
+  (todos: Todo[], filter: string) => {
+    switch (filter) {
+      default:
+      case 'SHOW_ALL':
+        return todos;
+      case 'SHOW_COMPLETED':
+        return todos.filter(t => t.completed);
+      case 'SHOW_ACTIVE':
+        return todos.filter(t => !t.completed);
+    }
   }
-});
+);
 
 export const getStateCompleted = createSelector(getTodos, (todos) => {
   return todos.every(todo => todo.completed);
