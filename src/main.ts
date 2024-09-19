8d49432e75ef616f27bc9521e3b263f9829657fc
Nicolas Molina Monroy <hi@nicobytes.com>
2024-09-19 04:55:40 -0400

    chore: change boostrap

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+
 import { environment } from './environments/environment';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { AppRoutingModule } from './app/app-routing.module';
+import { AppComponent } from './app/app.component';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+    providers: [importProvidersFrom(BrowserModule, AppRoutingModule)]
+})
   .catch(err => console.error(err));
